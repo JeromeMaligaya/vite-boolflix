8fb6426a5859f0100e8e579e49d65896a4dbd519
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-20 16:57:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+  @use "./style/general.scss" as *;
+
+</style>
