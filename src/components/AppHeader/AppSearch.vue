8d583aa4e3c0260bff031f9616e725fa77951677
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-21 16:23:08 +0200

    added getFilmsList() and datas on AppSearch

--- src/components/AppHeader/AppSearch.vue
+++ src/components/AppHeader/AppSearch.vue
@@ -1,23 +1,46 @@
 <script>
+import axios from 'axios';
+
 export default {
   data(){
     return{
         searchedFilm: "",
+        listFilms:[],
+        apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=ffdc9af86d5c7f48e60450138dfdd802",
     }
   },
   components: {
 
   },
   methods: {
+    getFilmsList(){
+        axios.get(this.apiUrl, {
+            params: {
+                query: this.searchedFilm,
+            }
+        })
+            .then(response => {
+                // handle success
+                console.log('response', response);
+                this.listFilms = response.data.results;
+                console.log('listFilms', this.listFilms)
 
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
   },
 }
 </script>
 
 <template>
     <header>
-        <input type="text" name="" id="" class="me-2" v-model="searchedFilm">
-        <button type="button">SEND</button>
+        
     </header>
 </template>
 
