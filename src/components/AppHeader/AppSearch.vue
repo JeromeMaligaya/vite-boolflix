8b15b41294fa051c637d66471c8c423a7f252633
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-23 11:49:03 +0200

    moved template and style of li.card-film from AppSearch to MainListFilmsElement

--- src/components/AppHeader/AppSearch.vue
+++ src/components/AppHeader/AppSearch.vue
@@ -43,41 +43,9 @@ export default {
     <header>
         <input type="text" name="" id="" class="me-2" v-model="store.searchedFilm" @keyup.enter="getFilmsList(store.searchedFilm)">
         <button type="button" @click="getFilmsList(store.searchedFilm)">SEND</button>
-        <ul>
-            <li v-for="(film, index) in store.listFilms" :key="index" class="card-film">
-                <p>film {{ index }}</p>
-                <p>title: {{ film.title }}</p>
-                <p>original-title: {{ film.original_title }}</p>
-                <div class="lang-film">
-                    <p>language: {{ film.original_language }}</p>
-                    <img :src="getLanguageFlag(film.original_language)" :alt="film.original_language" />
-                </div>
-                <p>vote: {{ parseFloat(film.vote_average).toFixed(2) }}</p>
-            </li>
-        </ul>
     </header>
 </template>
 
 <style lang="scss">
-
-    .card-film{
-        border: 2px black dashed;
-        margin-bottom: 20px;
-        p{
-            margin: 0;
-            padding: 0;
-        }
-
-        .lang-film{
-            display: flex;
-            align-items: center;
-            gap: 5px;
-
-            img{
-                width: 25px;
-            }
-        }
-    }
-
 
 </style>
--- src/components/AppMain/MainListFilmsElement.vue
+++ src/components/AppMain/MainListFilmsElement.vue
@@ -17,9 +17,36 @@ export default {
 </script>
 
 <template>
-
+  <li v-for="(film, index) in store.listFilms" :key="index" class="card-film">
+    <p>film {{ index }}</p>
+    <p>title: {{ film.title }}</p>
+    <p>original-title: {{ film.original_title }}</p>
+    <div class="lang-film">
+        <p>language: {{ film.original_language }}</p>
+        <img :src="getLanguageFlag(film.original_language)" :alt="film.original_language" />
+    </div>
+    <p>vote: {{ parseFloat(film.vote_average).toFixed(2) }}</p>
+  </li>
 </template>
 
 <style lang="scss" scoped>
+  
+  .card-film{
+        border: 2px black dashed;
+        margin-bottom: 20px;
+        p{
+            margin: 0;
+            padding: 0;
+        }
 
+        .lang-film{
+            display: flex;
+            align-items: center;
+            gap: 5px;
+
+            img{
+                width: 25px;
+            }
+        }
+    }
 </style>
