78552f68b49610c28238571b52dcbe8d694a845d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-23 10:56:07 +0200

    moved data of AppSearch to store.js

--- src/components/AppHeader/AppSearch.vue
+++ src/components/AppHeader/AppSearch.vue
@@ -1,39 +1,11 @@
 <script>
 import axios from 'axios';
+import { store } from '../../store.js'
 
 export default {
   data(){
     return{
-        searchedFilm: "",
-        listFilms:[],
-        apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=ffdc9af86d5c7f48e60450138dfdd802",
-        languagesFLags: [
-            {
-                en: "https://img.icons8.com/?size=100&id=xapj7ZzAUZKI&format=png&color=000000",
-            },
-            {
-                it: "https://img.icons8.com/?size=100&id=JfBHeXaPw7Gu&format=png&color=000000",
-            },
-            {
-                de: "https://img.icons8.com/?size=100&id=OyqucOGoByl9&format=png&color=000000",
-            },
-            {
-                fr: "https://img.icons8.com/?size=100&id=5RtaKEr09Jy6&format=png&color=000000",
-            },
-            {
-                es: "https://img.icons8.com/?size=100&id=nWg9OOe3Oyy0&format=png&color=000000",
-            },
-            {
-                pt: "https://img.icons8.com/?size=100&id=U7gojzqjuzog&format=png&color=000000",
-            },
-            {
-                zh: "https://img.icons8.com/?size=100&id=OafC2pWK4RV4&format=png&color=000000",
-            },
-            {
-                ja: "https://img.icons8.com/?size=100&id=KvglG3FkCenH&format=png&color=000000",
-            },
-
-        ]
+        store,
     }
   },
   components: {
@@ -41,16 +13,16 @@ export default {
   },
   methods: {
     getFilmsList(){
-        axios.get(this.apiUrl, {
+        axios.get(store.apiUrl, {
             params: {
-                query: this.searchedFilm,
+                query: store.searchedFilm,
             }
         })
         .then(response => {
             // handle success
             console.log('response', response);
-            this.listFilms = response.data.results;
-            console.log('listFilms', this.listFilms)
+            store.listFilms = response.data.results;
+            console.log('listFilms', store.listFilms)
 
         })
         .catch(function (error) {
@@ -63,7 +35,7 @@ export default {
 
     },
     getLanguageFlag(element){
-    const flagFinded = this.languagesFLags.find(flag => flag[element]);
+    const flagFinded = store.languagesFLags.find(flag => flag[element]);
 
     return flagFinded ? flagFinded[element] : 'https://img.icons8.com/?size=100&id=5tk64ASe7tdw&format=png&color=000000';
     },
@@ -74,10 +46,10 @@ export default {
 
 <template>
     <header>
-        <input type="text" name="" id="" class="me-2" v-model="searchedFilm" @keyup.enter="getFilmsList(searchedFilm)">
-        <button type="button" @click="getFilmsList(searchedFilm)">SEND</button>
+        <input type="text" name="" id="" class="me-2" v-model="store.searchedFilm" @keyup.enter="getFilmsList(store.searchedFilm)">
+        <button type="button" @click="getFilmsList(store.searchedFilm)">SEND</button>
         <ul>
-            <li v-for="(film, index) in listFilms" :key="index" class="card-film">
+            <li v-for="(film, index) in store.listFilms" :key="index" class="card-film">
                 <p>film {{ index }}</p>
                 <p>title: {{ film.title }}</p>
                 <p>original-title: {{ film.original_title }}</p>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,35 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+    searchedFilm: "",
+    listFilms: [],
+    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=ffdc9af86d5c7f48e60450138dfdd802",
+    languagesFLags: [
+        {
+            en: "https://img.icons8.com/?size=100&id=xapj7ZzAUZKI&format=png&color=000000",
+        },
+        {
+            it: "https://img.icons8.com/?size=100&id=JfBHeXaPw7Gu&format=png&color=000000",
+        },
+        {
+            de: "https://img.icons8.com/?size=100&id=OyqucOGoByl9&format=png&color=000000",
+        },
+        {
+            fr: "https://img.icons8.com/?size=100&id=5RtaKEr09Jy6&format=png&color=000000",
+        },
+        {
+            es: "https://img.icons8.com/?size=100&id=nWg9OOe3Oyy0&format=png&color=000000",
+        },
+        {
+            pt: "https://img.icons8.com/?size=100&id=U7gojzqjuzog&format=png&color=000000",
+        },
+        {
+            zh: "https://img.icons8.com/?size=100&id=OafC2pWK4RV4&format=png&color=000000",
+        },
+        {
+            ja: "https://img.icons8.com/?size=100&id=KvglG3FkCenH&format=png&color=000000",
+        },
+
+    ]
 
 })
