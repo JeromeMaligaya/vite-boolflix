1e4480c3071525f63e54789a695c2b10e329ac60
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-24 17:06:03 +0200

    moved languagesFLags[] and getLanguageFlag() from store.js to MainListFilmsElement

--- src/components/AppMain/MainListFilmsElement.vue
+++ src/components/AppMain/MainListFilmsElement.vue
@@ -5,6 +5,33 @@ export default {
   data(){
     return{
       store,
+      languagesFLags: [
+        {
+            en: "https://img.icons8.com/?size=100&id=xapj7ZzAUZKI&format=png&color=000000",
+        },
+        {
+            it: "https://img.icons8.com/?size=100&id=JfBHeXaPw7Gu&format=png&color=000000",
+        },
+        {
+            de: "https://img.icons8.com/?size=100&id=OyqucOGoByl9&format=png&color=000000",
+        },
+        {
+            fr: "https://img.icons8.com/?size=100&id=5RtaKEr09Jy6&format=png&color=000000",
+        },
+        {
+            es: "https://img.icons8.com/?size=100&id=nWg9OOe3Oyy0&format=png&color=000000",
+        },
+        {
+            pt: "https://img.icons8.com/?size=100&id=U7gojzqjuzog&format=png&color=000000",
+        },
+        {
+            zh: "https://img.icons8.com/?size=100&id=OafC2pWK4RV4&format=png&color=000000",
+        },
+        {
+            ja: "https://img.icons8.com/?size=100&id=KvglG3FkCenH&format=png&color=000000",
+        },
+
+    ],
     }
   },
   props:{
@@ -17,6 +44,11 @@ export default {
 
   },
   methods: {
+    getLanguageFlag(element) {
+        const flagFinded = this.languagesFLags.find(flag => flag[element]);
+
+        return flagFinded ? flagFinded[element] : 'https://img.icons8.com/?size=100&id=5tk64ASe7tdw&format=png&color=000000';
+    },
     getImageUrl(mediaPath) {
       const imageUrl = 'http://image.tmdb.org/t/p/w500/';
       return mediaPath ? imageUrl + mediaPath : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9H39OhrjpTZZkqqCRVV3rzhrtGVpvi6IWKg&s';
@@ -36,7 +68,7 @@ export default {
     <p>original-title: {{ media.original_title ? media.original_title : media.original_name }}</p>
     <div class="lang-media">
         <p>language: {{ media.original_language }}</p>
-        <img :src="store.getLanguageFlag(media.original_language)" :alt="media.original_language" />
+        <img :src="getLanguageFlag(media.original_language)" :alt="media.original_language" />
     </div>
     <p>vote: {{ getStarsValue(media.vote_average) }}</p>
   </li>
--- src/store.js
+++ src/store.js
@@ -7,37 +7,4 @@ export const store = reactive({
     listMedias: [],
     apiUrlFilms: "https://api.themoviedb.org/3/search/movie?api_key=ffdc9af86d5c7f48e60450138dfdd802",
     apiUrlSeries: "https://api.themoviedb.org/3/search/tv?api_key=ffdc9af86d5c7f48e60450138dfdd802",
-    languagesFLags: [
-        {
-            en: "https://img.icons8.com/?size=100&id=xapj7ZzAUZKI&format=png&color=000000",
-        },
-        {
-            it: "https://img.icons8.com/?size=100&id=JfBHeXaPw7Gu&format=png&color=000000",
-        },
-        {
-            de: "https://img.icons8.com/?size=100&id=OyqucOGoByl9&format=png&color=000000",
-        },
-        {
-            fr: "https://img.icons8.com/?size=100&id=5RtaKEr09Jy6&format=png&color=000000",
-        },
-        {
-            es: "https://img.icons8.com/?size=100&id=nWg9OOe3Oyy0&format=png&color=000000",
-        },
-        {
-            pt: "https://img.icons8.com/?size=100&id=U7gojzqjuzog&format=png&color=000000",
-        },
-        {
-            zh: "https://img.icons8.com/?size=100&id=OafC2pWK4RV4&format=png&color=000000",
-        },
-        {
-            ja: "https://img.icons8.com/?size=100&id=KvglG3FkCenH&format=png&color=000000",
-        },
-
-    ],
-    getLanguageFlag(element) {
-        const flagFinded = store.languagesFLags.find(flag => flag[element]);
-
-        return flagFinded ? flagFinded[element] : 'https://img.icons8.com/?size=100&id=5tk64ASe7tdw&format=png&color=000000';
-    },
-
 })
